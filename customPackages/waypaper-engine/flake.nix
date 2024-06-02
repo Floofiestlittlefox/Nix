5fe2dec4a6daa47324559b83328757a14150ee5e
lachlan <lachlanleoknell@gmail.com>
2024-06-02 17:45:08 +1000

    flake for waypaper-engine

--- customPackages/waypaper-engine/flake.nix
+++ customPackages/waypaper-engine/flake.nix
@@ -1,20 +1,33 @@
 {
 	description = "waypaper-engine";
 	inputs.waypaper-engine = {
+			flake=false;
 			type = "git";
 			url = "https://github.com/0bCdian/Waypaper-Engine";
 			rev = "a8f3a7e401ae211a27d014a7e8aeb06673036cfd";
 		};
 
-	outputs = {self, nixpkgs, fetchFromGithub, waypaper-engine}:
-		stdenv.mkDerivation {
-		name = "waypaper-engine";
+	outputs = {self, nixpkgs, waypaper-engine }: {
+		packages.x86_64-linux.waypaper-engine =
+		let
+		pkgs = import nixpkgs { system = "x86_64-linux"; };
+		in
+		pkgs.stdenv.mkDerivation {
+			name = "waypaper-engine";
 
-		src = inputs.waypaper-engine
+			src = waypaper-engine;
 
-		buildInputs = [pkgs.nodejs];
-		buildPhase = ''
-		'';
+			buildInputs = [pkgs.nodejs];
+			buildPhase = ''
+				  npm run build
+				  install -m 444 -D release/linux-unpacked/resources/icons/512x512.png $out/share/icons/hicolor/512x512/apps/waypaper-engine.png
 
-	}
+				  install -m 444 -D release/linux-unpacked -rt $out/opt/waypaper-engine
+				  install -m 744 cli/waypaper-engine $out/bin
+				  install -m 444 -D waypaper-engine.desktop $out/share/applications
+
+			'';
+
+		};
+	};
 }
