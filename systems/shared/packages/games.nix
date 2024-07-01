857ff782fc394625035b02115525fcea9ac928a6
Lachlan <lachlanleoknell@gmail.com>
2024-07-02 01:05:49 +1000

    latexfix

--- systems/shared/packages/games.nix
+++ systems/shared/packages/games.nix
@@ -1,4 +1,18 @@
-{...}:
+{pkgs, lib, config, ...}:
 {
-  programs.steam.enable = true;
+  programs = {
+    steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+    };
+    java.enable = true;
+  };
+  hardware.opengl = {
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+  environment.systemPackages = 
+  with pkgs; [
+    cataclysm-dda   
+  ];
 }
